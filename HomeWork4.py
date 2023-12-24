39eec826d1efddf6faeea0652c17b26845d82ad8
VladocV <148562894+VladocV@users.noreply.github.com>
2023-12-24 22:46:04 +0300

    Update HomeWork4.py

--- HomeWork4.py
+++ HomeWork4.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
+#чтение датасета
 df = pd.read_csv('/content/Customers.csv', delimiter=';')
 
+#поиск людей у которых возраст больше 30 и доход меньше 30000
 df[(df['Age'] > 30) & (df['Income'] < 30000)]
 
+#поиск людей, которые по профессии юристы с опытом работы больше 5 лет
 df[(df['Profession'] == 'Lawyer') & (df['Work Experience'] > 5)]
