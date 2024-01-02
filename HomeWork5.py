c12fbee770be44eeccbbef00c67ee17db6d9e110
VladocV <148562894+VladocV@users.noreply.github.com>
2024-01-02 15:11:44 +0300

    Update HomeWork5.py

--- HomeWork5.py
+++ HomeWork5.py
@@ -1,6 +1,13 @@
+import pandas as pd
 
- - - - - - - - - - - - 
+# Загрузка данных из датасета
+df = pd.read_csv('/content/Customers.csv', delimiter=';')
 
-=======================
-Здесь пока ничего нет(
-=======================
+# Удаление строк с пропусками
+missing_values = df.isnull().sum()
+df = df.dropna()
+
+# Группировка и вычисление среднего дохода
+income_profes = df.groupby('Profession')['Income'].mean()
+income_profes = round(income_profes)
+print(f'Средний годовой доход по профессиям:\n{income_profes}')
